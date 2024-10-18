8c0853af0463ac96581668e7202f3b1aca51735f
Helge <helgepeggen@web.de>
2024-10-18 09:35:55 +0200

    Add rnd_boolean

---
+++ example.py
@@ -0,0 +1,13 @@
+import random
+
+def rnd_ganzzahl(start, end):
+    return int(random.random() * (end - start + 1) + start)
+
+def rnd_boolean():
+    zahl = rnd_ganzzahl(0, 100)
+    if zahl % 2 == 0:
+        return True
+    else:
+        return False
+    
+print(rnd_boolean())
