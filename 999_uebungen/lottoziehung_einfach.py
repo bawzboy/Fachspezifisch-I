2fb61d62fdcdffffcb8bb5f739cbbfc1c698256d
bawzboy <helgepeggen@web.de>
2024-10-19 17:50:42 +0200

    Add pool of numbers

--- 999_uebungen/lottoziehung_einfach.py
+++ 999_uebungen/lottoziehung_einfach.py
@@ -10,16 +10,14 @@ Gebe die Zahlen aus, sobald 6 unterschiedliche Zahlen vorhanden sind
 from random import randint
 
 def main():
+    lotto_topf = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49]
+    
     lotto_ziehung = []
-    while len(lotto_ziehung) < 6:        
-            zahl = lotto_zahl()
-            if zahl not in lotto_ziehung:
+    while len(lotto_ziehung) < 6:
+            zahl = randint(1, 49)
+            if zahl in lotto_topf and zahl not in lotto_ziehung:
                 lotto_ziehung.append(zahl)
     print(sorted(lotto_ziehung))
-
-
-def lotto_zahl():
-    return randint(1, 49)
 
 if __name__ == '__main__':
     main()
