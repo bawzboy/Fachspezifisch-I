da2da155ae22ddd564cee1c6080a73ea07227204
Helge <helgepeggen@web.de>
2024-10-24 11:57:46 +0200

    .

---
+++ 999_uebungen/db_start.py
@@ -0,0 +1,7 @@
+import sqlite3
+
+
+connection = sqlite3.connect('erste_db.db')
+
+cursor = connection.cursor()
+
---
+++ 999_uebungen/quartett.py
@@ -0,0 +1,7 @@
+'''
+- verschiedene Karten (Karten Klasse? alt. Dictionary)
+- Kategorien mit vergleichbaren Werten
+- mehrere Spieler(min 2)?
+- Stapel zum Nachziehen
+- 
+'''
